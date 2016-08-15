ae9b40815c71c975cee81e0d1ebf7227d01b7afc
jochenparm <jochen.parmentier@heraeus.com>
2016-08-15 19:13:02 +0200

    Added unstaged packages

---
+++ Test/FunctionalLibrary/Navigator/Settings/RecycleBin/RecycleBin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.FunctionalLibrary.Navigator.Settings.RecycleBin
+{
+    public class RecycleBin : NavigatorBase, IRecycleBin
+    {
+        public RecycleBin()
+        {
+            MailWasher.UiMap.MainWindow.Tabs.Settings.RecycleBin_Button.Click();
+        }
+
+        public General.IGeneral General
+        {
+            get
+            {
+                return new General.General();
+            }
+        }
+
+        public Restoring.IRestoring Restoring
+        {
+            get
+            {
+                return new Restoring.Restoring();
+            }
+        }
+    }
+}
